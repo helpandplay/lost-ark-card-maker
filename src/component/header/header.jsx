85d7c123971d704200203d48b0599240bbe85980
helpandplay <helpandplay.kr@gmail.com>
2020-11-09 23:57:21 +0900

    add image maker link

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -3,6 +3,11 @@ import styles from "./header.module.css";
 const Header = ({ user, onLogout }) => {
   return (
     <div className={styles.header}>
+      {user && (
+        <a target="_blank" href="/image-maker">
+          이미지 생성기
+        </a>
+      )}
       <h1>LostArk Card Generator</h1>
       {user && (
         <button onClick={onLogout} className={styles.logout}>
